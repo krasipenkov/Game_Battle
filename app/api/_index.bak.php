fc4eebe1c857dd2af152fc12ab5f2b91ea05218e
unknown <test@example.com>
2015-07-04 18:15:57 +0300

    rest server initial commit

---
+++ app/api/_index.bak.php
@@ -0,0 +1,26 @@
+
+<?
+
+require_once '../config/config.inc.php';
+require_once '../config/error.consts.php';
+
+require_once LIB_PATH.'db/DB.php';
+require_once ENTITIES_PATH.'User.php';
+require_once API_PATH.'Response.php';
+
+use app\config\Config;
+use app\lib\db\DB;
+use app\api\Response;
+
+DB::getInstance();
+
+
+
+use app\entities\User;
+
+$user = new User();
+$data = ($user->getUser());
+$response = new Response($data);
+
+echo $response->getResponse();
+
