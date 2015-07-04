be7e62c1722319630a671247aa555b87eb2c1a17
buglow <buglov.kostadin@gmail.com>
2015-07-04 15:02:47 +0300

    :)

--- app/node/src/server_rest.js
+++ app/node/src/server_rest.js
@@ -1,23 +1,23 @@
+var request = require('sync-request');
+
 var rest = {};
 
 rest.url = 'http://localhost/test_api.php';
 	
-rest.send =function(method, object, object_id, params)
+rest.send = function(method, object, object_id, params)
 {
 	if(!object_id)
 		object_id = '';
 	
 	var url = this.url+object+'/'+object_id;
 	
-	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.open( method, url, true );
-	
-	http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	http.setRequestHeader("Content-length", params.length);
-	http.setRequestHeader("Connection", "close");
-	
-	xmlHttp.send( params );
-	return xmlHttp.responseText;
+	var res = request(method, 'http://localhost/test_api.html');
+	if( res.statusCode == '200' )
+	{
+		return res.body;
+	}
+
+	return false;
 };
 
 rest.get = function(object, object_id)
