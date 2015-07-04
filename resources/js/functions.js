e2ab43579808aa0f2d084d1e026eb13ed360f0c2
Ivan Ivanov <itanev@gmail.com>
2015-07-04 13:37:46 +0300

    lobby message

---
+++ resources/js/functions.js
@@ -0,0 +1,9 @@
+/* Extract GET parameter */
+function gup(name, url) {
+  if (!url) url = location.href
+  name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+  var regexS = "[\\?&]"+name+"=([^&#]*)";
+  var regex = new RegExp( regexS );
+  var results = regex.exec( url );
+  return results == null ? null : results[1];
+}
